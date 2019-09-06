b72961114b465bcf4b72e57f9f656fcd13762c2c
inbound-dev <54907540+inbound-dev@users.noreply.github.com>
2019-09-06 13:46:23 -0400

    Add files via upload

---
+++ AllAboutMeJava.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package allaboutme.java;
+
+
+/**
+ *
+ * @author joham3169
+ */
+public class AllAboutMeJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int age = 15;
+       
+        System.out.println("My Name is:");
+        System.out.println("Jordan Hamilton");
+        System.out.println("My Age Is:");
+        System.out.println(age);
+        System.out.println("One Of My Hobbies Is:");
+        System.out.println("Coding");
+        System.out.println("My Youtube Channel Is Called");
+        System.out.println("Inbound");
+    }
+    
+}
